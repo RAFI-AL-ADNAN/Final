eec4bad3595383e84b7aa278c088111a15c61a6f
RAFI-AL-ADNAN <108135015+RAFI-AL-ADNAN@users.noreply.github.com>
2024-04-26 23:18:27 +0600

    gdafdgfad

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ document.getElementById('searchBtn').addEventListener('click', function() {
         searchMeal(searchText);
     }
 });
+function searchMeal(keyword) {
+    let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${keyword}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            displayMeals(data.meals);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
